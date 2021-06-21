fd205cc5306dcd33f15378316c5180cc48e76779
Jordan Ball <cjordanball@ballwebdev.com>
2021-06-20 23:43:29 -0400

    fix up constraints and alignments of elements

--- How Long/ViewController.swift
+++ How Long/ViewController.swift
@@ -6,13 +6,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    let eggTimes: [String: Int] = [
+        "Soft": 60 * 3,
+        "Medium": 60 * 4,
+        "Hard": 60 * 5
+    ];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet weak var mainTitle: UILabel!
+    
+    
+    var time = Timer();
+    var hardness: String = "";
+    var player: AVAudioPlayer!;
+    
+    func playSound() {
+        let soundURL = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3");
+        
+        player = try! AVAudioPlayer(contentsOf: soundURL!);
+        player.play();
+    }
+    
+    
+
+    
+    
+
 
 
 }
