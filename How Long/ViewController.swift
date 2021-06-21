ff71a44450d78862fadee8add7708cf1677e2c36
Jordan Ball <cjordanball@ballwebdev.com>
2021-06-21 08:54:35 -0400

    add timer functionality

--- How Long/ViewController.swift
+++ How Long/ViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
     let eggTimes: [String: Int] = [
-        "Soft": 60 * 3,
-        "Medium": 60 * 4,
-        "Hard": 60 * 5
+        "Soft": 6 * 3,
+        "Medium": 6 * 4,
+        "Hard": 6 * 5
     ];
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        Bar.progress = 0.0;
+        mainTitle.text = "How Do You Like Eggs?";
         // Do any additional setup after loading the view.
     }
     
     @IBOutlet weak var mainTitle: UILabel!
+    @IBOutlet weak var Bar: UIProgressView!
     
     
-    var time = Timer();
+    var timer = Timer();
     var hardness: String = "";
     var player: AVAudioPlayer!;
     
@@ -35,11 +39,25 @@ class ViewController: UIViewController {
     }
     
     
-
-    
-    
-
-
-
-}
+    @IBAction func eggHandler(_ sender: UIButton) {
+        timer.invalidate();
+        if (sender.currentTitle != hardness) {
+            if let hardness = sender.currentTitle {
+                let hardNum = Float(eggTimes[hardness]!);
+                var timeSet = hardNum;
+                timer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { timer in
+                    self.Bar.progress = max((hardNum - Float(timeSet)) / hardNum, 0.0);
+                        timeSet -= 0.2;
+                        if (timeSet <= 0) {
+                            timer.invalidate();
+                            self.playSound();
+                            self.mainTitle.text = "Done!";
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+//}
 
